365609cb01b673a4ba197d9b52132489d54ec0f7
wkd-woo <wkdwoos@gmail.com>
2022-02-03 23:13:16 +0900

    220203

--- dynamic_programming/1010.py
+++ dynamic_programming/1010.py
@@ -1,8 +1,12 @@
-import itertools
+import math
+
+
 
 T = int(input())
 
 for i in range(T):
     N, M = map(int, input().split())
     if 0 < N <= M < 30:
-        print(len(list(itertools.combinations(range(M), N))))
+        # result = M!/(M - N)!(N!)
+        result = math.factorial(M) // (math.factorial(M-N) * math.factorial(N))
+        print(result)
