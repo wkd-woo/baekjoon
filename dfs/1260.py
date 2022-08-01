9c5d433c93bc2b64fcfcfc0823f0044d2a85d221
wkd-woo <wkdwoos@gmail.com>
2022-08-01 16:43:59 +0900

    220801

--- dfs/1260.py
+++ dfs/1260.py
@@ -1,54 +1,41 @@
 from collections import deque
 
 
-def dfs(graph, start_node):
-    visited = []
-    stack = deque()
-
-    stack.append(start_node)
-
-    while stack:
-        node = stack.pop()
-
-        if node not in visited:
-            visited.append(node)
-            stack += reversed(graph[node])
-
-    for each in visited:
-        print(each, end=' ')
-
-
 def bfs(graph, start_node):
-    queue, visited = deque(), list()
+    visited = list()
+    queue = deque()  # need_visit
+
     queue.append(start_node)
 
     while queue:
-        node = queue.popleft()
-
+        node = queue.popleft()  # deque로 선언하여 popleft()
         if node not in visited:
             visited.append(node)
             queue.extend(graph[node])
 
-    for each in visited:
-        print(each, end=' ')
+    return visited
 
+def dfs(graph, start_node):
+    visited, stack = list(), list()
 
-# 정점, 간선, 시작점 입력
-points, lines, start = map(int, input().split())
+    stack.append(start_node)
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+            stack.extend(reversed(graph[node]))
 
-if 1 <= points <= 1000 and 1 <= lines <= 10000:
+    return visited
 
-    # 이차원 배열의 행을 정점 개수만큼
-    graph = {i: [] for i in range(1, points + 1)}
+n, m, v = map(int, input().split())
+graph = {i: [] for i in range(1, n + 1)}
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-    for _ in range(1, lines + 1):
-        node_1, node_2 = map(int, input().split())
-        graph[node_1].append(node_2)
-        graph[node_2].append(node_1)
+for key in graph:
+    graph[key].sort()
 
-    for key in graph:
-        graph[key].sort()
-
-    dfs(graph, start)
-    print()
-    bfs(graph, start)
+print(*dfs(graph, v))
+print(*bfs(graph, v))
